a83f841666ff603bd17a9288bdf5e9937e396640
syania28 <kamilaputri543@gmail.com>
2024-10-24 09:43:19 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,25 @@
 public class Nomor1{
     public static void main(String[] args){
+import java.util.Scanner;
+public class asesment {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner;
+
+        //input variable
+        int jamLembur= 20;
+        int HonorLembur= 12000000;
+        int Totaljamkerja= 12;
+        int honorlembur= 22000000;
+        
+        //menghitung besaran honor lembur
+       if ("jamLembur >=16 jam"); {
+       } else; ("int 10000/jam");
+
+        if ("jamLembur=honor> 16 jam");{  
+        } else ("int 15000/jam");{
+
+        //menghitung honorer
+        ("honor lembur=22000";)
 
     }
 }

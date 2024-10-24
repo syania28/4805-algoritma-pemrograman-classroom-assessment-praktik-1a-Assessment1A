13399222c075995facd1cb1db3c56d9c4b69dfaf
syania28 <kamilaputri543@gmail.com>
2024-10-24 09:46:11 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,31 @@
 public class Nomor2{
     public static void main(String[] args){
+import java.util.Scanner;
+public class asesment2 {
+    public static void main(String[] args) {
+    Scanner scanner= new Scanner("");
+
+    //masukkan variable
+    int Lamatrip;
+    int biayaPerKilometer= 13000;
+    int Totaltrip= 11;
+    
+    //masukkan nama [Driver,PakHasan,Mereka,Kita]
+    System.out.println("Masukkan nama Beliau");
+    input next()=("Beliau");
+    System.out.println("Masukkan nama Kita");
+    input nextLine("kita");
+    System.out.println("Masukkan nama Mereka");
+    input nextline()= ("Mereka");
+    
+    //Total Omzet perjalanan
+    System.out.println("Lamatrip= 4km");
+    System.out.println("Lamatrip= 5km");
+    System.out.println("Lamatrip=2km");
+
+    (Totaltrip=11;)
+
+
 
     }
 }
